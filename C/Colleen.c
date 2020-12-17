183d6c063fe6efeb1a760e6a980765fd9e5c13e2
Kilian Botrel <botrel.kilian1999@gmail.com>
2020-12-17 09:58:04 +0100

    Made printf parameters a bit more readable

--- C/Colleen.c
+++ C/Colleen.c
@@ -6,9 +6,9 @@
 
 void quine()
 {
-    char* string ="/*%c    This is the first comment.%c*/%c%c#include <stdio.h>%c%cvoid quine()%c{%c    char* string =%c%s%c;%c%c    printf(string,10,10,10,10,10,10,10,10,34,string,34,10,10,10,10,10,10,10,10,10,10,10,10,10,10);%c}%c%cint main()%c{%c    /*%c        This is the second comment.%c    */%c%c    quine();%c}%c";
+    char* string ="/*%c    This is the first comment.%c*/%c%c#include <stdio.h>%c%cvoid quine()%c{%c    char* string =%c%s%c;%c%c    printf(string, 10, 10, 10, 10, 10, 10, 10, 10, 34, string, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main()%c{%c    /*%c        This is the second comment.%c    */%c%c    quine();%c}%c";
 
-    printf(string,10,10,10,10,10,10,10,10,34,string,34,10,10,10,10,10,10,10,10,10,10,10,10,10,10);
+    printf(string, 10, 10, 10, 10, 10, 10, 10, 10, 34, string, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
 
 int main()
