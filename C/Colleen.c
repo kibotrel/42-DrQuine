473c2ebbedfb5810819a1fb1527f38b33d267869
Kilian Botrel <botrel.kilian1999@gmail.com>
2020-12-17 09:14:58 +0100

    C - Colleen Quine

---
+++ C/Colleen.c
@@ -0,0 +1,21 @@
+/*
+    This is the first comment.
+*/
+
+#include <stdio.h>
+
+void quine()
+{
+    char* string ="/*%c    This is the first comment.%c*/%c%c#include <stdio.h>%c%cvoid quine()%c{%c    char* string =%c%s%c;%c%c    printf(string,10,10,10,10,10,10,10,10,34,string,34,10,10,10,10,10,10,10,10,10,10,10,10,10,10);%c}%c%cint main()%c{%c    /*%c        This is the second comment.%c    */%c%c    quine();%c}%c";
+
+    printf(string,10,10,10,10,10,10,10,10,34,string,34,10,10,10,10,10,10,10,10,10,10,10,10,10,10);
+}
+
+int main()
+{
+    /*
+        This is the second comment.
+    */
+
+    quine();
+}
