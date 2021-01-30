92a6529c2d3e552c3bc0a946e296b240413bc379
Kilian Botrel <botrel.kilian1999@gmail.com>
2021-01-30 22:05:40 +0100

    Fixed a bug with exec that didn't have a stop condition

--- JS/Sully.js
+++ JS/Sully.js
@@ -7,7 +7,10 @@ const main = () => {
   if (i > 0 && __filename.indexOf('Sully.js') === -1) { i-- }
 
   fs.writeFile(`Sully_${i}.js`, `let i = ${i}\n\nconst main = ${main.toString()}\n\nmain()\n`, (error) => { if (error) throw error })
-  exec(`node Sully_${i}.js`, (error) => { if (error) throw error })
+  
+  if (i > 0) {
+    exec(`node Sully_${i}.js`, (error) => { if (error) throw error })
+  }
 }
 
 main()
